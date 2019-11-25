ede7aab5b5bd2f2b8a3e091bab257a2254e5468d
Zack Amin <ishaqhamin@gmail.com>
2019-11-25 15:29:29 +0000

    Initial Commit

---
+++ DiceGameApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DiceGameApp
+//
+//  Created by Ishaq on 25/11/2019.
+//  Copyright © 2019 Ishaq. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
